16f0ae7a2c9dd1edfc95a9e0bd91dd45f0b7658e
kugaichen <chenyuanfeng0101@163.com>
2024-04-14 20:03:00 +0800

    complete hw06

---
+++ hw/hw06/hw06.py
@@ -0,0 +1,214 @@
+""" Homework 6: OOP and Inheritance"""
+
+#####################
+# Required Problems #
+#####################
+
+class VendingMachine:
+    """A vending machine that vends some product for some price.
+
+    >>> v = VendingMachine('candy', 10)
+    >>> v.vend()
+    'Machine is out of stock.'
+    >>> v.add_funds(15)
+    'Machine is out of stock. Here is your $15.'
+    >>> v.restock(2)
+    'Current candy stock: 2'
+    >>> v.vend()
+    'You must add $10 more funds.'
+    >>> v.add_funds(7)
+    'Current balance: $7'
+    >>> v.vend()
+    'You must add $3 more funds.'
+    >>> v.add_funds(5)
+    'Current balance: $12'
+    >>> v.vend()
+    'Here is your candy and $2 change.'
+    >>> v.add_funds(10)
+    'Current balance: $10'
+    >>> v.vend()
+    'Here is your candy.'
+    >>> v.add_funds(15)
+    'Machine is out of stock. Here is your $15.'
+
+    >>> w = VendingMachine('soda', 2)
+    >>> w.restock(3)
+    'Current soda stock: 3'
+    >>> w.restock(3)
+    'Current soda stock: 6'
+    >>> w.add_funds(2)
+    'Current balance: $2'
+    >>> w.vend()
+    'Here is your soda.'
+    """
+    "*** YOUR CODE HERE ***"
+    def __init__(self,name,number):
+        self.name = name
+        self.stock = 0 
+        self.number = 0
+        self.number_limit = number
+        self.tryhands = 0
+    
+    def vend(self):
+        if self.number ==0 and self.tryhands ==0:
+            self.tryhands = self.tryhands + 1 
+            return('Machine is out of stock.')
+        elif self.number > self.number_limit:
+            value = self.number - self.number_limit
+            self.number = 0 
+            return('Here is your {0} and ${1} change.'.format(self.name,value))
+            
+        elif self.number == self.number_limit:
+            return('Here is your {0}.'.format(self.name))     
+            
+        elif self.number < self.number_limit:
+            return('You must add ${0} more funds.'.format(self.number_limit - self.number))        
+        
+    def restock(self,num):
+        self.stock = self.stock + num
+        return('Current {0} stock: {1}'.format(self.name,self.stock))        
+                 
+    def add_funds(self,num):
+        if num < 15 :
+            self.number = self.number + num 
+            return('Current balance: ${0}'.format(self.number))
+        else:
+           return('Machine is out of stock. Here is your ${0}.'.format(num))
+    
+
+class Pet:
+    """A pet.
+
+    >>> kyubey = Pet('Kyubey', 'Incubator')
+    >>> kyubey.talk()
+    Kyubey
+    >>> kyubey.eat('Grief Seed')
+    Kyubey ate a Grief Seed!
+    """
+    def __init__(self, name, owner):
+        self.is_alive = True    # It's alive!!!
+        self.name = name
+        self.owner = owner
+    
+    def eat(self, thing):
+        print(self.name + " ate a " + str(thing) + "!")
+    
+    def talk(self):
+        print(self.name)
+        
+
+
+class Cat(Pet):
+    """A cat.
+
+    >>> vanilla = Cat('Vanilla', 'Minazuki Kashou')
+    >>> isinstance(vanilla, Pet) # check if vanilla is an instance of Pet.
+    True
+    >>> vanilla.talk()
+    Vanilla says meow!
+    >>> vanilla.eat('fish')
+    Vanilla ate a fish!
+    >>> vanilla.lose_life()
+    >>> vanilla.lives
+    8
+    >>> vanilla.is_alive
+    True
+    >>> for i in range(8):
+    ...     vanilla.lose_life()
+    >>> vanilla.lives
+    0
+    >>> vanilla.is_alive
+    False
+    >>> vanilla.lose_life()
+    Vanilla has no more lives to lose.
+    """
+    def __init__(self, name, owner, lives=9):
+        "*** YOUR CODE HERE ***"
+        self.name = name
+        self.lives = lives
+        self.is_alive = True
+
+    def talk(self):
+        """ Print out a cat's greeting.
+        """
+        "*** YOUR CODE HERE ***"
+        print('Vanilla says meow!')
+
+    def lose_life(self):
+        """Decrements a cat's life by 1. When lives reaches zero, 'is_alive'
+        becomes False. If this is called after lives has reached zero, print out
+        that the cat has no more lives to lose.
+        """
+        "*** YOUR CODE HERE ***"
+        if self.lives:
+            self.lives = self.lives - 1 
+            if self.lives == 0:
+                self.is_alive = False
+        else:
+            print('Vanilla has no more lives to lose.')
+            
+
+
+
+class NoisyCat(Cat): # Dose this line need to change?
+    """A Cat that repeats things twice.
+
+    >>> chocola = NoisyCat('Chocola', 'Minazuki Kashou')
+    >>> isinstance(chocola, Cat) # check if chocola is an instance of Cat.
+    True
+    >>> chocola.talk()
+    Chocola says meow!
+    Chocola says meow!
+    """
+
+    
+    def talk(self):
+        """Talks twice as much as a regular cat.
+        """
+        "*** YOUR CODE HERE ***"
+        print('Chocola says meow!')
+        print('Chocola says meow!')
+
+
+##########################
+# Just for fun Questions #
+##########################
+
+class Fib:
+    """A Fibonacci number.
+
+    >>> start = Fib()
+    >>> start.value
+    0
+    >>> start.next().value
+    1
+    >>> start.next().next().value
+    1
+    >>> start.next().next().next().value
+    2
+    >>> start.next().next().next().next().value
+    3
+    >>> start.next().next().next().next().next().value
+    5
+    >>> start.next().next().next().next().next().next().value
+    8
+    >>> start.value # Ensure start isn't changed
+    0
+    """
+
+    def __init__(self, value=0):
+        self.value = value
+
+    def next(self):
+        "*** YOUR CODE HERE ***"
+        if self.value == 0:
+            next_fib = Fib(1)
+        
+        else:
+            next_fib = Fib(self.previous + self.value)
+            
+        next_fib.previous = self.value 
+        
+        return next_fib
+            
+    
