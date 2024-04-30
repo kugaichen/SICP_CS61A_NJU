b99edb320e5544d445d1e99b8b3088cbb8e9f514
kugaichen <chenyuanfeng0101@163.com>
2024-04-30 16:35:36 +0800

    complete lab08

--- lab/lab08/lab08.scm
+++ lab/lab08/lab08.scm
@@ -1,17 +1,22 @@
 ;;; Lab08: Scheme
 
 (define (over-or-under a b)
-  'YOUR-CODE-HERE
+  (cond
+    ((> a b) '1)
+    ((< a b) '-1)
+    ((= a b) '0)
+
+  )
 )
 
 
 (define (make-adder n)
-  'YOUR-CODE-HERE
+  (lambda (x) (+ x n))
 )
 
 
 (define (composed f g)
-  'YOUR-CODE-HERE
+  (lambda (x)(f (g x)))
 )
 
 
@@ -19,15 +24,18 @@
   (- a (* b (quotient a b))))
 
 (define (gcd a b)
-  'YOUR-CODE-HERE
+  (if (= b 0) a
+    (gcd b (remainder a b)))
 )
 
 
 (define lst
-  'YOUR-CODE-HERE
+  (cons (cons 1 nil) (cons 2 (cons (cons 3 (cons 4 nil)) (cons 5 nil))))
 )
 
 
 (define (ordered s)
-  'YOUR-CODE-HERE
-)
+  (if (null? (cdr s)) #t
+    (if (<= (car s) (car (cdr s))) (ordered(cdr s)) #f)  
+  )
+) 
--- lab/lab08/tests/combinations.py
+++ lab/lab08/tests/combinations.py
@@ -7,76 +7,54 @@ test = {
         {
           'code': r"""
           scm> (- 10 4)
-          f43a983c0daf6c1d050dbe0833c15187
-          # locked
+          6
           scm> (* 7 6)
-          9f2845c08838e3d74a4a825544551738
-          # locked
+          42
           scm> (+ 1 2 3 4)
-          3ec9872f57999918aff6256a4468475f
-          # locked
+          10
           scm> (/ 8 2 2)
-          12ff846b5df2ea6bda9ece2c27d4e07d
-          # locked
+          2
           scm> (quotient 29 5)
-          14b862d1cc1ac887a348e7877803d91c
-          # locked
+          5
           scm> (modulo 29 5)
-          35335a4cb21b81bbdd7c9fde450ac2ba
-          # locked
+          4
           scm> (= 1 3)                    ; Scheme uses '=' instead of '==' for comparison
-          1d68a21f5e10a0b34e0e72d9ed47796f
-          # locked
+          #f
           scm> (< 1 3)
-          2d7cbeef49a0612644978f75b0aff19d
-          # locked
+          #t
           scm> (or 1 #t)                  ; or special form short circuits
-          ab602226889628a880f560d548c9532f
-          # locked
+          1
           scm> (and #t #f (/ 1 0))
-          1d68a21f5e10a0b34e0e72d9ed47796f
-          # locked
+          #f
           scm> (not #t)
-          1d68a21f5e10a0b34e0e72d9ed47796f
-          # locked
+          #f
           scm> (define x 3)
-          556d7474b05028c5f33a8dbe76672010
-          # locked
+          x
           scm> x
-          331378a62fb7d46a780484bd24dd9bc4
-          # locked
+          3
           scm> (define y (+ x 4))
-          6b3fe8d55a9adf0d4813e02496ddad7d
-          # locked
+          y
           scm> y
-          d389f94a5f34049b2a7d5ca263c63628
-          # locked
+          7
           scm> (define x (lambda (y) (* y 2)))
-          556d7474b05028c5f33a8dbe76672010
-          # locked
+          x
           scm> (x y)
-          92a7d397a794074ba6a711853088fe9d
-          # locked
+          14
           scm> (if (not (print 1)) (print 2) (print 3))
-          ab602226889628a880f560d548c9532f
-          331378a62fb7d46a780484bd24dd9bc4
-          # locked
+          1
+          3
           scm> (* (if (> 3 2) 1 2) (+ 4 5))
-          6b20a778cd26fa9470e9c7c675e76925
-          # locked
+          9
           scm> (define foo (lambda (x y z) (if x y z)))
-          cd6dbf3fc5fa76db00b4a63b6f47a298
-          # locked
+          foo
           scm> (foo 1 2 (print 'hi))
-          e5a021da3be17dba99a7c815cf974c33
-          12ff846b5df2ea6bda9ece2c27d4e07d
-          # locked
+          hi
+          2
           scm> ((lambda (a) (print 'a)) 100)
-          cc8f26e398984759770a00540b27b1c9
-          # locked
+          a
           """,
           'hidden': False,
-          'locked': True,
+          'locked': False,
           'multiline': False
         }
       ],
